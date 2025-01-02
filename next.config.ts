59ab4eb5bce15a91c5f9c32da555141bfba4ede8
Malo Misson <malo@embie.be>
2025-01-02 18:57:12 +0100

    log

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default withNextIntl(nextConfig);
